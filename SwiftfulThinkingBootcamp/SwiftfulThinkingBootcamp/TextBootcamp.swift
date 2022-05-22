c28f8b0b58a63ac3cce02847bdf1b4038e0a8a44
jahongir <jah6332@gmail.com>
2022-05-22 16:49:50 +0500

    TextBootCamp

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/TextBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  TextBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Jahongir on 22/05/22.
+//
+
+import SwiftUI
+
+struct TextBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextBootcamp()
+    }
+}
